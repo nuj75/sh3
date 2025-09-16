eb1adc664eaea88d9c2d5b3445a88404cbfe2d94
nuj75 <anujan.kandasamy@gmail.com>
2025-09-15 20:40:32 -0400

    first commit

---
+++ assignment1/assignment1.c
@@ -0,0 +1,46 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+#include <linux/jiffies.h>
+
+ssize_t proc_read(struct file *file, char *buf, size_t, count, loff_t *pos)
+{
+    int result = 0;
+    char buffer[BUFFER_SIZE];
+
+    static int complete = 0;
+
+    if (complete)
+    {
+        complete = 0;
+        return 0;
+    }
+
+    complete = 1;
+
+    result = sprintf(buffer, jiffies / HZ);
+
+    return rv;
+}
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = proc_read,
+}
+
+int
+proc_start(void)
+{
+    proc_create("seconds", 0, NULL, &proc_ops);
+
+    return 0;
+}
+
+int proc_quit(void)
+{
+    remove_proc_entry("seconds", NULL);
+}
+module_init(proc_start);
+module_exit(proc_quit);
