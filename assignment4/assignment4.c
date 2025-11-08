088723a8fbd366f9ecbfb814ce801037197ecdc8
nuj75 <anujan.kandasamy@gmail.com>
2025-11-08 00:34:20 -0500

    add tlb things

--- assignment4/assignment4.c
+++ assignment4/assignment4.c
@@ -2,29 +2,37 @@
 #include <sys/mman.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-// should this be unsigned or can it be signed
+struct TLBStruct
+{
+    signed char page_number;
+    signed char frame_number;
+};
 
-unsigned char page_table[256] = {0};
-unsigned char memory[32768] = {0};
+struct TLBStruct *tlb_arr[16] = {NULL};
+int tlb_tail = 0;
+
+signed char page_table[256] = {0};
+signed char memory[32768] = {0};
 int memory_tail = 0;
 
 signed char *init_backstore()
 {
     int file = open("BACKING_STORE.bin", O_RDONLY);
-    unsigned char *mmap_pointer = mmap(0, 65536, PROT_READ, MAP_PRIVATE, file, 0);
+    signed char *mmap_pointer = mmap(0, 65536, PROT_READ, MAP_PRIVATE, file, 0);
 
     return mmap_pointer;
 }
 
-void clear_backstore(unsigned char *mmap_pointer)
+void clear_backstore(signed char *mmap_pointer)
 {
     munmap(mmap_pointer, 65536);
 }
 
-unsigned char translate_page(unsigned char page_number, unsigned char *backstore_mmap)
+signed char translate_page(signed char page_number, signed char *backstore_mmap)
 {
-    unsigned char page_hit = page_table[page_number];
+    signed char page_hit = page_table[page_number];
 
     if (page_hit & 0xFE == 1)
     {
@@ -32,21 +40,68 @@ unsigned char translate_page(unsigned char page_number, unsigned char *backstore
     }
 
     memcpy(memory + memory_tail, backstore_mmap + 256 * page_number, 256);
-    unsigned char frame_number = memory_tail >> 8;
+    signed char frame_number = memory_tail >> 8;
     memory_tail = (memory_tail + 256) % 32768;
 
     return frame_number;
 }
+
+signed char search_TLB(signed char page_number)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        if (tlb_arr[(tlb_tail + 16 - i) % 16] != NULL && tlb_arr[(tlb_tail + 16 - i) % 16]->page_number == page_number)
+        {
+            return tlb_arr[(tlb_tail + 16 - i) % 16]->frame_number;
+        }
+    }
+
+    return -1;
+}
+
+void add_TLB(signed char page_number, signed char frame_number)
+{
+    struct TLBStruct *new_entry = malloc(sizeof(struct TLBStruct));
+    new_entry->page_number = page_number;
+    new_entry->frame_number = frame_number;
+
+    free(tlb_arr[tlb_tail]);
+
+    tlb_arr[tlb_tail] = new_entry;
+
+    tlb_tail = (tlb_tail + 1) % 16;
+}
+
+void TLB_update(signed char old_page_number, signed char new_page_number)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        if (tlb_arr[(tlb_tail + 16 - i) % 16] != NULL && tlb_arr[(tlb_tail + 16 - i) % 16]->page_number == old_page_number)
+        {
+            tlb_arr[(tlb_tail + 16 - i) % 16]->page_number = new_page_number;
+        }
+    }
+}
+
 int main()
 {
-    unsigned char *backstore_mmap = init_backstore();
+    signed char *backstore_mmap = init_backstore();
 
     FILE *fptr = fopen("addresses.txt", "r");
 
-    unsigned char buffer[2];
-    while (fgets(buffer, 2, fptr) != NULL)
+    char buffer[100];
+
+    while (fgets(buffer, sizeof(buffer), fptr) != NULL)
     {
-        printf("%X\n", translate_page(buffer[0], backstore_mmap));
+        buffer[strlen(buffer) - 2] = '\0';
+
+        int virtual_addr = atoi(buffer);
+        signed char page_number = virtual_addr >> 8;
+        signed char offset = (virtual_addr & 0x00FF);
+
+        signed char physical_address = (translate_page(page_number, backstore_mmap) << 8) + offset;
+
+        // printf("%X\n", physical_address);
     }
 
     clear_backstore(backstore_mmap);
